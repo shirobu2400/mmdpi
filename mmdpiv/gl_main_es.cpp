8a0e3a97e7e64a2f1ce7e1dda7c26bac3efd8e1e
shirobu2400 <shirobu_2400kskm@yahoo.co.jp>
2017-11-03 22:40:55 +0900

    update 20171103

--- mmdpiv/gl_main_es.cpp
+++ mmdpiv/gl_main_es.cpp
@@ -610,7 +610,8 @@ int main( int argc, char *argv[] )
 		frames ++;
 
 		//glutTimerFunc( fps->get_wait_time() * 1000.0f, timer, 0 );
-		usleep( 1700 );
+		//usleep( 1700 );
+		usleep( 3200 );
 		if( debug_flag )
 			fps->draw();
 	}
